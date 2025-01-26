6f9fa9e900145c3642b7a0c298e52b577c0d4da3
rohitcod3 <rohit.bangar9@gmail.com>
2025-01-26 15:11:21 +0530

    Added cache structure

---
+++ proxy_server_with_cache.c
@@ -0,0 +1,26 @@
+#include "proxy_parse.h"
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <pthread.h>
+
+define MAX_CLIENTS 10 typedef struct cache_element cache_element;
+
+struct cache_element
+{
+    char *data;
+    int len;
+    char *url;
+    time_t lru_time_track;
+    cache_element *next;
+};
+
+cache_element *find(char *url);
+int add_cache_element(char *data, int size, char *url);
+void remove_cache_element();
+
+int port_number = 8080;
+int proxy_socketId;
+pthread_t tid[MAX_CLIENTS];
+sem_t semaphore;
+pthread_mutex_t lock;
