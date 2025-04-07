97f091327e80c85aa22ce2bd13a6c9dcb3a153ac
rohitcod3 <rohit.bangar9@gmail.com>
2025-04-06 23:45:35 -0400

    Add connectRemoteServer and handleRequest functions to forward client requests to remote server

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -4,10 +4,16 @@
 #include <time.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-#define MAX_CLIENTS 10 typedef struct cache_element cache_element;
+#define MAX_CLIENTS 10
+typedef struct cache_element cache_element;
 
-#define MAX_BYTES 4096;
+#define MAX_BYTES 4096
+
 struct cache_element
 {
     char *data;
@@ -31,12 +37,84 @@ cache_element *head;
 
 int cache_size;
 
+int connectRemoteServer(char *host_addr, int port_num) {
+    int remoteSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (remoteSocket < 0) {
+        printf("Error in creating your socket\n");
+        return -1;
+    }
+
+    struct hostent *host = gethostbyname(host_addr);
+    if (host == NULL) {
+        fprintf(stderr, "No such host exists\n");
+        return -1;
+    }
+
+    struct sockaddr_in server_addr;
+    bzero((char *)&server_addr, sizeof(server_addr));  // Fixed: should be server_addr
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_num);
+
+    bcopy((char *)host->h_addr, (char *)&server_addr.sin_addr.s_addr, host->h_length); // Fixed: host->h_length (not host->h_lenght)
+
+    if (connect(remoteSocket, (struct sockaddr *)&server_addr, (socklen_t)sizeof(server_addr)) < 0) {
+        fprintf(stderr, "Error in connecting\n"); // Fixed: fprintf, not fprint
+        return -1;
+    }
+
+    return remoteSocket;
+}
+
+int handle_request(int clientSocketId, ParsedRequest *request, char *tempReq) {
+    int remoteSocketId = connectRemoteServer(request->host, 80);port 80
+    if (remoteSocketId < 0) {
+        return -1;
+    }
+
+    char buf[MAX_BYTES];
+    int bytes_send = send(remoteSocketId, tempReq, strlen(tempReq), 0);
+    bzero(buf, MAX_BYTES);
+
+    bytes_send = recv(remoteSocketId, buf, MAX_BYTES - 1, 0);
+
+    char *temp_buffer = (char *)malloc(sizeof(char) * MAX_BYTES); 
+    int temp_buffer_size = MAX_BYTES;
+    int temp_buffer_index = 0;
+
+    while (bytes_send > 0) {
+        send(clientSocketId, buf, bytes_send, 0);
+
+        for (int i = 0; i < bytes_send; i++) { 
+            temp_buffer[temp_buffer_index] = buf[i]; /
+            temp_buffer_index++;
+        }
+
+        temp_buffer_size += MAX_BYTES;
+        temp_buffer = (char *)realloc(temp_buffer, temp_buffer_size); 
+
+        if (bytes_send < 0) {
+            perror("Error in sending data to the client\n");
+            break;
+        }
+
+        bzero(buf, MAX_BYTES);
+        bytes_send = recv(remoteSocketId, buf, MAX_BYTES - 1, 0);
+    }
+
+    free(temp_buffer);
+    close(remoteSocketId);
+
+    return 0;
+}
+
 void *thread_fn(void *socketNew)
 {
     sem_wait(&semaphore);
     int p;
-    sem_getValue(&semaphore, p);
+    sem_getvalue(&semaphore, &p);
     printf("semaphore value is: %d\n", p);
+
     int *t = (int *)socketNew;
     int socket = *t;
     int bytes_send_client, len;
@@ -64,18 +142,21 @@ void *thread_fn(void *socketNew)
     {
         tempReq[i] = buffer[i];
     }
+    tempReq[strlen(buffer)] = '\0'; // Null terminate manually
+
     struct cache_element *temp = find(tempReq);
     if (temp != NULL)
     {
         int size = temp->len / sizeof(char);
         int pos = 0;
         char response[MAX_BYTES];
+
         while (pos < size)
         {
             bzero(response, MAX_BYTES);
-            for (int i = 0; i < MAX_BYTES; i++)
+            for (int i = 0; i < MAX_BYTES && pos < size; i++)
             {
-                response[i] = temp->data[i];
+                response[i] = temp->data[pos];
                 pos++;
             }
             send(socket, response, MAX_BYTES, 0);
@@ -98,38 +179,78 @@ void *thread_fn(void *socketNew)
             {
                 if (request->host && request->path && checkHTTPversion(request->version) == 1)
                 {
-                    bytes_send_client = handle_request();
+                    bytes_send_client = handle_request(socket, request, tempReq);
+                    if (bytes_send_client == -1)
+                    {
+                        sendErrorMessage(socket, 500);
+                    }
+                    else
+                    {
+                        sendErrorMessage(socket, 500);
+                    }
+                }
+                else
+                {
+                    printf("this code doesnt support any method apart from GET\n");
                 }
             }
+            ParsedRequest_destroy(request);
         }
+
+        if (bytes_send_client == 0)
+        {
+            printf("client is disconnected");
+        }
+
+        shutdown(socket, SHUT_RDWR);
+        close(socket);
+        free(buffer);
+        sem_post(&semaphore);
+        sem_getvalue(&semaphore, &p);
+        printf("semaphore post value is %d\n", p);
+        free(tempReq);
+        return NULL;
     }
+
+    shutdown(socket, SHUT_RDWR);
+    close(socket);
+    free(buffer);
+    sem_post(&semaphore);
+    sem_getvalue(&semaphore, &p);
+    printf("semaphore post value is %d\n", p);
+    free(tempReq);
+    return NULL;
 }
 
 int main(int argc, char *argv[])
 {
     int client_socketId, client_len;
     struct sockaddr_in server_addr, client_addr;
+
     sem_init(&semaphore, 0, MAX_CLIENTS);
     pthread_mutex_init(&lock, NULL);
-    if (argv == 2)
+
+    if (argc == 2)
     {
         port_number = atoi(argv[1]);
     }
     else
     {
-        printf("Too few arguments");
+        printf("Too few arguments\n");
         exit(1);
     }
 
     printf("Starting Proxy server at port: %d\n", port_number);
+
     proxy_socketId = socket(AF_INET, SOCK_STREAM, 0);
     if (proxy_socketId < 0)
     {
         perror("Failed to create a socket\n");
         exit(1);
     }
+
     int reuse = 1;
-    if (setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (cont char *)&reuse, sizeof(reuse)) < 0)
+    if (setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (const char *)&reuse, sizeof(reuse)) < 0)
     {
         perror("setSockOpt failed\n");
     }
@@ -138,18 +259,22 @@ int main(int argc, char *argv[])
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port_number);
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    if (bind(proxy_socketId, (struct sockaddr *)&server_add, sizeof(server_addr) < 0))
+
+    if (bind(proxy_socketId, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         perror("Port is not available\n");
         exit(1);
     }
-    printf("Binding on port %d\n" port_number);
+
+    printf("Binding on port %d\n", port_number);
+
     int listen_status = listen(proxy_socketId, MAX_CLIENTS);
     if (listen_status < 0)
     {
         perror("Error in listening\n");
         exit(1);
     }
+
     int i = 0;
     int Connected_socketId[MAX_CLIENTS];
 
@@ -157,11 +282,11 @@ int main(int argc, char *argv[])
     {
         bzero((char *)&client_addr, sizeof(client_addr));
         client_len = sizeof(client_addr);
-        client_socketId = accept(proxy_socketId, (struct * sockaddr_in) & client_addr, (socketlen_t *)&client_len);
+        client_socketId = accept(proxy_socketId, (struct sockaddr *)&client_addr, (socklen_t *)&client_len);
         if (client_socketId < 0)
         {
-            printf("Not able to connect");
-            exit(1);
+            printf("Not able to connect\n");
+            continue;
         }
         else
         {
@@ -169,14 +294,17 @@ int main(int argc, char *argv[])
         }
 
         struct sockaddr_in *client_ot = (struct sockaddr_in *)&client_addr;
-        struct in_addr ip_addr = client_pt->sin_addr;
-        char str[INET_ADDSTRLEN];
-        inet_nstop(AF_INTET, &ip_addr, str, INET_ADDRSTRLEN);
-        printf("Client is connected with port number %d and ip address is %s\n", ntohs(client_addr.sin_port), str);
+        struct in_addr ip_addr = client_ot->sin_addr;
+        char str[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &ip_addr, str, INET_ADDRSTRLEN);
+
+        printf("Client is connected with port number %d and ip address is %s\n",
+               ntohs(client_addr.sin_port), str);
 
         pthread_create(&tid[i], NULL, thread_fn, (void *)&Connected_socketId[i]);
-        i++;
+        i = (i + 1) % MAX_CLIENTS;
     }
+
     close(proxy_socketId);
     return 0;
 }
